c9831fa345ecd87989b76ac60cced2bbfa1ac8c2
RoBoTz <104291850+RoBoTz001@users.noreply.github.com>
2022-04-29 07:36:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ Ek = Client(
 @Ek.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
     TEXT = "Hai {} \n\n**I Am Lyrics Search Bot. Send Me A Song Name, I Will Give You The Lyrics. ** \n\nFor Know More /help"
-    BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton("Channel 🔰", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("Support Group ⭕️", url = "https://telegram.me/ekbotz_support")],[InlineKeyboardButton("Repo 🗂️", url = "https://github.com/M-fazin/Lyrics-Search-Bot"),InlineKeyboardButton("Deploy 🗃️", url = "https://heroku.com/deploy?template=https://github.com/M-fazin/Lyrics-Search-Bot")],[InlineKeyboardButton("Developer 💡", url = "https://github.com/M-fazin/")]])
+    BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton("Channel 🔰", url = "https://t.me/MM_CINEMA_CLUB"),InlineKeyboardButton("Support Group ⭕️", url = "https://t.me/Moviez_group_mg")],[InlineKeyboardButton("Repo 🗂️", url = "https://github.com/RoBoTz001/Lyrics-Search-Bot"),InlineKeyboardButton("Deploy 🗃️", url = "https://heroku.com/deploy?template=https://github.com/RoBoTz001/Lyrics-Search-Bot")],[InlineKeyboardButton("Developer 💡", url = "https://t.me/cinemaclubcc")]])
     await update.reply_text(
         text=TEXT.format(update.from_user.mention),
         reply_markup=BUTTONS,
@@ -31,7 +31,7 @@ async def start(bot, update):
 @Ek.on_message(filters.private & filters.command(["help"]))
 async def help(bot, update):
     HELP = "Hai {} \n\n**There Is Nothing To Know More.** \n- Send Me A Song Name, I Will Give Lyrics Of That Song. \nBot By @EKBOTZ_UPDATE "
-    HELP_BUTTON = InlineKeyboardMarkup([[InlineKeyboardButton("🧑‍💻 Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("🗃️ Source Code", url = "https://github.com/M-fazin/Lyrics-Search-Bot")]])
+    HELP_BUTTON = InlineKeyboardMarkup([[InlineKeyboardButton("🧑‍💻 Channel", url = "https://t.me/MM_CINEMA_CLUB"),InlineKeyboardButton("🗃️ Source Code", url = "https://github.com/M-fazin/Lyrics-Search-Bot")]])
     await update.reply_text(
         text=HELP.format(update.from_user.mention),
         reply_markup=HELP_BUTTON,
@@ -41,7 +41,7 @@ async def help(bot, update):
 	
 @Ek.on_message(filters.private & filters.command(["about", "source", "repo"]))
 async def about(bot, update):
-    ABOUT = "**🤖 Bot :** Lyrics Search Bot\n\n**🧑‍💻 Developer :** [M-fazin](https://github.com/M-fazin)\n\n**💻 Channel :** @EKBOTZ_UPDATE\n\n**☎️ Support :** @ekbotz_support \n\n**🗂️ Source Code :** [Lyrics Search Bot](https://github.com/M-fazin/Lyrics-Search-Bot)\n\n**⚙️ Language :** Python 3\n\n**🛡️ Framework :** Pyrogram"
+    ABOUT = "**🤖 Bot :** Lyrics Search Bot\n\n**🧑‍💻 Developer :** [M-fazin](https://t.me/cinemaclubcc)\n\n**💻 Channel :** @MM_CINEMA_CLUB\n\n**☎️ Support :** @Moviez_group_mg \n\n**🗂️ Source Code :** [Lyrics Search Bot](https://github.com/RoBoTz001/Lyrics-Search-Bot)\n\n**⚙️ Language :** Python 3\n\n**🛡️ Framework :** Pyrogram"
     await update.reply_text(
 	text=ABOUT,
 	disable_web_page_preview=True,
@@ -71,7 +71,7 @@ def lyrics(song):
         fin = search(song)
         text = f'**🎶 Successfully Extracted Lyrics Of {song} 🎶**\n\n\n\n'
         text += f'`{fin["lyrics"]}`'
-        text += '\n\n\n**Made With ❤️ By @EKBOTZ_UPDATE**'
+        text += '\n\n\n**Made With ❤️ By @Cinema_Club_Post**'
         return text
 
 
